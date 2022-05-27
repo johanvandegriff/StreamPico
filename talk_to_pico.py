5f2d2bd049dc361203d9ad77d5d27b8a61adb255
Johan Vandegriff <johan@vandymail.com>
2022-05-26 22:23:09 -0400

    break out json into a config file

--- talk_to_pico.py
+++ talk_to_pico.py
@@ -6,7 +6,7 @@ import traceback
 from queue import Queue
 from threading import Thread
 from gnome_wayland_monitor_active_app import monitor_active_app
-
+import json
 
 start_page = '1'
 current_page = start_page
@@ -14,179 +14,8 @@ OBS_PASSWORD = '1234' #TODO env var or config value
 SERIAL_INTERFACE = '/dev/rfcomm0'
 BAUD_RATE = 115200 #9600
 
-pages = {
-    '1': {
-        'apps': ['org.wezfurlong.wezterm.desktop', 'librewolf.desktop'],
-#         'color': 'RGB,255,0,0',
-        'keys': {
-            '0': {
-                #'colors': ['RGB,255,0,0', 'RGB,255,255,255'],
-                'colors': {
-                    None: 'RGB,128,128,0',
-                    'lights off! (toggled)': 'RGB,128,0,0',
-                    'lights on! (toggled)': 'RGB,255,255,255',
-                },
-                #TODO ability to shortcut:
-                #'action': '/home/user/nextcloud/bin/lights-toggle'
-                'action': {
-                    'type': 'command',
-                    'parameters': ['/home/user/nextcloud/bin/lights-toggle'],
-                },
-            },
-            '1': {
-                'colors': {
-                    None: 'RGB,128,128,0',
-                    'relay off! (toggled)': 'RGB,128,0,0',
-                    'relay on! (toggled)': 'RGB,255,255,255',
-                },
-                'action': {
-                    'type': 'command',
-                    'parameters': ['/home/user/nextcloud/bin/relay-toggle'],
-                },
-            },
-            #'4': {
-                #'colors': 'RGB,128,0,128',
-                #'action': {
-                    #'type': 'command',
-                    #'parameters': ['/home/user/nextcloud/bin/weznethack'],
-                #},
-            #},
-            #'5': {
-                #'colors': 'RGB,0,0,128',
-                #'action': {
-                    #'type': 'command',
-                    #'parameters': ['/home/user/nextcloud/bin/weznethack', 'number_pad'],
-                #},
-            #},
-            'E': { #TODO maybe use C as back instead of E
-                'colors': 'RGB,0,0,255',
-                'action': {
-                    'type': 'set_page',
-                    'parameters': ['None'],
-                },
-            },
-            'F': {
-                'colors': 'RGB,0,255,0',
-                'action': {
-                    'type': 'set_page',
-                    'parameters': ['2'],
-                },
-            },
-        },
-        'quit': 'E', #a special action that is managed on the device
-    },
-    '2': {
-        'apps': ['org.kde.kate.desktop', 'org.kde.dolphin.desktop', 'com.obsproject.Studio.desktop'],
-        'color': 'RGB,255,0,255',
-        'keys': {
-            '0': {
-                'action': {
-                    'type': 'command',
-                    #TODO a way to store values in the json config and reference them later, something like:
-                    #'vars': {'OBS_PASSWORD': '1234'}
-                    #'parameters': ['obs-cli', '--password', {'var': 'OBS_PASSWORD'}, 'scene', 'switch', 'cam+chat'],
-                    #TODO a way to pass the state (maybe previous output, maybe page and button number) into the command:
-                    #'parameters': ['my-command', {'state': 'LAST_OUTPUT'}],
-                    'parameters': ['obs-cli', '--password', OBS_PASSWORD, 'scene', 'switch', 'cam+chat'],
-                },
-            },
-            '1': {
-                'action': {
-                    'type': 'command',
-                    'parameters': ['obs-cli', '--password', OBS_PASSWORD, 'scene', 'switch', 'screen+cam+chat'],
-                },
-            },
-            '2': {
-                'action': {
-                    'type': 'command',
-                    'parameters': ['obs-cli', '--password', OBS_PASSWORD, 'scene', 'switch', '1080+cam+chat+vmpk'],
-                },
-            },
-            '3': {
-                'action': {
-                    'type': 'command',
-                    'parameters': ['obs-cli', '--password', OBS_PASSWORD, 'scene', 'switch', 'split screen'],
-                },
-            },
-            '4': {
-                'action': {
-                    'type': 'command',
-                    'parameters': ['obs-cli', '--password', OBS_PASSWORD, 'scene', 'switch', 'be right back'],
-                },
-            },
-            '5': {
-                'action': {
-                    'type': 'command',
-                    'parameters': ['obs-cli', '--password', OBS_PASSWORD, 'scene', 'switch', 'stream ending'],
-                },
-            },
-            '6': {
-                'action': {
-                    'type': 'command',
-                    'parameters': ['obs-cli', '--password', OBS_PASSWORD, 'scene', 'switch', 'vine'],
-                },
-            },
-            '7': {
-                'action': {
-                    'type': 'command',
-                    'parameters': ['obs-cli', '--password', OBS_PASSWORD, 'scene', 'switch', 'airhorn'],
-                },
-            },
-            '8': {
-                'action': {
-                    'type': 'command',
-                    'parameters': ['obs-cli', '--password', OBS_PASSWORD, 'scene', 'switch', 'sir vander'],
-                },
-            },
-            '9': {
-                'action': {
-                    'type': 'command',
-                    'parameters': ['obs-cli', '--password', OBS_PASSWORD, 'scene', 'switch', 'sir vander cam'],
-                },
-            },
-            #'A': {
-                #'action': {
-                    #'type': 'command',
-                    #'parameters': ['obs-cli', '--password', OBS_PASSWORD, 'scene', 'switch', '?'],
-                #},
-            #},
-            #'B': {
-                #'action': {
-                    #'type': 'command',
-                    #'parameters': ['obs-cli', '--password', OBS_PASSWORD, 'scene', 'switch', '?'],
-                #},
-            #},
-            'A': {
-                'colors': 'RGB,0,0,0',
-            },
-            'B': {
-                'colors': 'RGB,0,0,0',
-            },
-            'C': {
-                'action': {
-                    'type': 'command',
-                    'parameters': ['obs-cli', '--password', OBS_PASSWORD, 'source', 'toggle-mute', 'komplete desktop'],
-                },
-            },
-            'D': {
-                'action': {
-                    'type': 'command',
-                    'parameters': ['obs-cli', '--password', OBS_PASSWORD, 'source', 'toggle-mute', 'komplete mic'],
-                },
-            },
-            'E': {
-                'colors': 'RGB,0,0,255',
-                'action': {
-                    'type': 'set_page',
-                    'parameters': ['1'],
-                },
-            },
-            'F': {
-                'colors': 'RGB,0,0,0',
-            },
-        },
-    },
-}
+with open('config.json', 'r') as f:
+    pages = json.load(f)
 
 apps_to_page_mapping = {}
 for page, v in pages.items():
@@ -248,7 +77,7 @@ def activate_page():
             if type(colors) == list:
                 pln(key+','+colors[0]) #TODO cycle thru when pressed
             elif type(colors) == dict:
-                pln(key+','+colors[action.get('last_output')])
+                pln(key+','+colors[str(action.get('last_output'))])
             elif colors is not None:
                 pln(key+','+colors)
     if 'quit' in page:
