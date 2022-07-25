1114a7961b1a41a0c28a4decf251d0c95f5e40e7
Johan Vandegriff <johan@vandymail.com>
2022-07-25 03:02:44 -0400

    added windows support

--- README.md
+++ README.md
@@ -1,31 +1,38 @@
 # StreamPico
-Use your Raspberry Pi Pico RGB Keypad to control OBS (or anything!) over bluetooth
+Use your [Raspberry Pi Pico RGB Keypad](https://shop.pimoroni.com/products/pico-rgb-keypad-base) to control [OBS](https://obsproject.com/) (or anything!) over bluetooth.
 
-## Bluetooth Serial on Arch
+## Linux Setup
+Install Python 3 and Git:
+```bash
+sudo apt install python3 git #debian/ubuntu based systems
+sudo pacman -Syy python3 git #arch based systems
+```
 
-https://wiki.archlinux.org/title/Bluetooth#Bluetooth_serial
-
-pip install git+https://github.com/pybluez/pybluez
+Set up bluetooth (skip if bluetooth already works):
+```bash
+yay -S bluez-utils-compat #only needed on Manjaro
 rfkill unblock bluetooth
-yay -S bluez-utils-compat
 sudo systemctl restart bluetooth
-bluetoothctl
 ```
-[bluetooth] power on
-[bluetooth] agent on
-[bluetooth] scan on
-[bluetooth] scan off
-[bluetooth] pair 98:D3:31:FD:5D:34
+
+## Windows Setup
+ - install python from [python.org](https://www.python.org/downloads/windows/)
+ - install git from [git-scm.com](https://git-scm.com/download/win)
+ - install C++ from [here](https://visualstudio.microsoft.com/visual-cpp-build-tools/) (needed for compiling a python package)
+
+## Common Setup
+Go to bluetooth settings and pair the device. It should show up as `HC-06`, and the PIN is `1234`. (You can change the device name and PIN by sending serial commands to the HC-06 bluetooth module, but that is not needed for now.)
+
+Open a terminal in Linux, or open "Git Bash" in Windows and run these commands:
+```bash
+git clone https://codeberg.org/johanvandegriff/StreamPico
+cd StreamPico
+pip install virtualenv
+python -m virtualenv .venv
+source .venv/*/activate
+pip install -r requirements.txt
+
+python talk_to_pico.py
 ```
-sudo rfcomm bind 0 98:D3:31:FD:5D:34
 
-./talk_to_pico.py
-
-
-
-to remove:
-sudo rfcomm unbind 0
-bluetoothctl
-```
-[bluetooth] remove 98:D3:31:FD:5D:34
-```
+On Windows, if you don't have Git Bash, you can use cmd, and instead of `source .venv/*/activate`, you can do: `.\.venv\Scripts\activate`
--- talk_to_pico.py
+++ talk_to_pico.py
@@ -6,15 +6,38 @@ import time
 import traceback
 from queue import Queue
 from threading import Thread
-from gnome_wayland_monitor_active_app import monitor_active_app
 import json
+import platform
+import appdirs
 
-with open(os.path.expanduser('~/.config/StreamPico.json'), 'r') as f:
+try:
+    import pygetwindow
+except:
+    #TODO contribute Linux (X11 and wayland) and Mac support to pygetwindow
+    pass
+
+# https://newbedev.com/reliably-detect-windows-in-python
+is_linux = platform.system() == 'Linux'
+
+DEFAULT_BLUETOOTH_NAME = 'HC-06'
+CONFIG_DIR = appdirs.user_config_dir()+'/StreamPico'
+CONFIG_FILE = CONFIG_DIR + '/config.json'
+
+if not os.path.exists(CONFIG_DIR):
+    os.mkdir(CONFIG_DIR)
+if not os.path.exists(CONFIG_FILE):
+    with open(CONFIG_FILE, 'w') as f:
+        json.dump({'pages': {'mypage': {'color': 'RGB,255,255,255'}}}, f, indent=4)
+
+#TODO documentation for configs, some example configs
+#TODO ui to change config
+with open(os.path.expanduser(appdirs.user_config_dir()+'/StreamPico/config.json'), 'r') as f:
     config = json.load(f)
-    constants = config['constants']
+    constants = config.get('constants', {})
     switch_on_active_app = config.get('switch_on_active_app') == True
-    pages = config['pages']
-    START_PAGE = config['start_page']
+    pages = config.get('pages', {})
+    first_page = list(pages.keys())[0] if len(pages) else None
+    START_PAGE = config.get('start_page', first_page) #start with the first page if none specified
     current_page = START_PAGE
 
 apps_to_page_mapping = {}
@@ -25,14 +48,14 @@ for page, v in pages.items():
             apps_to_page_mapping[app] = page
 
 
-#new thread to listen to gnome events and find when the active window changes
-#so that the keypad can change context based on the active window
 q = Queue()
-if switch_on_active_app:
+if switch_on_active_app and is_linux:
+    from gnome_wayland_monitor_active_app import monitor_active_app
+    #new thread to listen to gnome events and find when the active window changes
+    #so that the keypad can change context based on the active window
     t = Thread(target=monitor_active_app, args=[q])
     t.daemon = True
     t.start()
-
 
 def find_device_by_name(name):
   print(f'searching for bluetooth device "{name}"...')
@@ -42,13 +65,13 @@ def find_device_by_name(name):
       print('name matches!')
       return addr
   print('no matches found')
-  return None
+  raise Exception(f'Bluetooth device {name} not found')
 
 
 if 'bluetooth_addr' in config:
   BLUETOOTH_ADDR = config['bluetooth_addr']
 else:
-  BLUETOOTH_ADDR = find_device_by_name(config['bluetooth_name'])
+  BLUETOOTH_ADDR = find_device_by_name(config.get('bluetooth_name', DEFAULT_BLUETOOTH_NAME))
 
 def pln(s):
     print('SEND', s)
@@ -57,8 +80,16 @@ def pln(s):
 RECV_DATA = b''
 def gln():
     global RECV_DATA
-    while b'\r\n' not in RECV_DATA:
-      RECV_DATA += sock.recv(1024)
+    try:
+        RECV_DATA += sock.recv(1024)
+    except bluetooth.btcommon.BluetoothError as e: #Linux
+        if e.args[0] != 'timed out':
+            raise e
+    except OSError as e:
+        if not 'did not properly respond after a period of time' in e.args[0]: #Windows
+            raise e
+    if b'\r\n' not in RECV_DATA:
+        return None
     lines = RECV_DATA.split(b'\r\n')
     line = lines[0].decode('utf-8')
     lines = lines[1:] #delete the 1st
@@ -81,10 +112,15 @@ def activate_page():
             colors = data.get('colors')
             action = data.get('action')
             if type(colors) == list:
-                pln(key+','+colors[0]) #TODO cycle thru when pressed
+                if len(colors) > 0:
+                    index = action.get('index', 0)
+                    pln(key+','+colors[index]) #TODO test: cycle thru when pressed
+                    action['index'] = (index + 1) % len(colors)
             elif type(colors) == dict:
-                pln(key+','+colors[str(action.get('last_output'))])
-            elif colors is not None:
+                last_output = str(action.get('last_output'))
+                if last_output in colors:
+                    pln(key+','+colors[last_output])
+            elif type(colors) == str:
                 pln(key+','+colors)
     if 'quit' in page:
         pln('Q,'+page['quit'])
@@ -94,9 +130,11 @@ def activate_page():
 
 send_init = True
 while True:
+    if switch_on_active_app and not is_linux:
+        q.put(pygetwindow.getActiveWindow().title)
     if not q.empty():
         app = q.get()
-        new_page = apps_to_page_mapping.get(app)
+        new_page = apps_to_page_mapping.get(app) #TODO regex or glob especially needed on windows
         #TODO add an optional catchall page for apps not on any list
         #TODO pattern matching for apps
         #TODO add an action to set/toggle if app focus jumps to page
@@ -108,10 +146,12 @@ while True:
         if send_init:
             sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
             sock.connect((BLUETOOTH_ADDR, 1))
+            sock.settimeout(0.05)
             pln("?")
             send_init = False
 
-        line = gln()
+        if (line := gln()) is None:
+            continue
 
         if line[0] == '.':
             pln('.')
@@ -122,13 +162,13 @@ while True:
             activate_page()
         elif line[0] == '+':
             key = line[1]
-            if current_page in pages and key in pages[current_page]['keys']:
+            if key in pages.get(current_page, {}).get('keys', {}):
                 action = pages[current_page]['keys'][key].get('action')
                 if action is not None:
                     print('action', action, 'type', type(action))
                     if action['type'] == 'set_page':
                         pln('#'+',' + ','.join(action['parameters']))
-                    elif action['type'] == 'command':
+                    elif action['type'] == 'command': #TODO direct obs-websocket support
                         print('command', action['parameters'])
                         parameters = []
                         for param in action['parameters']:
@@ -139,15 +179,23 @@ while True:
                                 if not constant in constants:
                                     raise ValueError(f"constant {constant} not defined")
                                 parameters.append(constants[constant])
-                        output = subprocess.Popen(parameters, stdout=subprocess.PIPE ).communicate()[0]
+                        try:
+                            output = subprocess.Popen(parameters, stdout=subprocess.PIPE).communicate()[0] #TODO non blocking option
+                        except Exception as e:
+                            output = bytes(str(e), 'utf-8')
+                            print(traceback.format_exc())
                         print('output', output)
                         pages[current_page]['keys'][key]['action']['last_output'] = output.decode('utf-8').strip()
                         activate_page() #TODO dont update the whole page
                     else:
                         raise Exception('unsupported action type ' + action['type'])
-    except bluetooth.btcommon.BluetoothError as e:
-        print(traceback.format_exc())
-        send_init = True
+    except bluetooth.btcommon.BluetoothError as e: #TODO windows cannot detect bluetooth disconnect
+        if e.args[0] == 110: #[Errno 110] Connection timed out
+            print(traceback.format_exc())
+            print("Attempting to reconnect...")
+            send_init = True
+        else:
+            raise e
 
 
 
